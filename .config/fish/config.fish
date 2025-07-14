9171554e65972376716a1969613c5b3d386ed033
Andrew Lee <aiaechel@gmail.com>
2025-07-14 00:47:32 -0700

    Add emacs alias and npm path var

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -91,3 +91,6 @@ alias jctl="journalctl -p 3 -xb"
 
 # Recent installed packages
 alias rip="expac --timefmt='%Y-%m-%d %T' '%l\t%n %v' | sort | tail -200 | nl"
+
+# PATH
+set -gx PATH ~/.npm-global/bin $PATH
