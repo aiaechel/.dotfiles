09c2a8922d7d80d1e839eaa018d6a2659d3d66d8
Andrew Lee <aiaechel@gmail.com>
2025-07-19 22:59:05 -0700

    Add power-menu.sh to let me shutdown my computer using rofi

---
+++ .config/rofi/scripts/power-menu.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Rofi Power Menu
+
+# Options
+options="Shutdown
+Restart
+Lock"
+
+# Get user choice
+chosen=$(echo -e "$options" | rofi -dmenu -p "Power")
+
+# Execute action based on choice
+case "$chosen" in
+    "Shutdown")
+        shutdown now
+        ;;
+    "Restart")
+        reboot
+        ;;
+    "Lock")
+        swaylock -f
+        ;;
+    *)
+        ;;
+esac
